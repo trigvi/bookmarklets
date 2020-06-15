dc13bc791f628f1e291b18e94322309df61bbc6c
Fabio Artuso <me@fabioartuso.com>
2020-06-15 19:04:48 +0000

    Las bookmarklet: using Textise

--- i24-bookmarklet.js
+++ i24-bookmarklet.js
@@ -1,2 +1,1 @@
 javascript:void function(){var e=document.createElement("script");e.src="https://cdn.jsdelivr.net/gh/trigvi/bookmarklets/i24.js",document.body.appendChild(e)}();
-
--- las-bookmarklet.js
+++ las-bookmarklet.js
@@ -1,2 +1,3 @@
-javascript:void function(){var e=document.createElement("script");e.src="https://cdn.jsdelivr.net/gh/trigvi/bookmarklets/las.js",document.body.appendChild(e)}();
-
+javascript:void function(){window.open("https://www.textise.net/showText.aspx?strURL="+encodeURI(window.location.href));}();
+// Old version that works with las.js (which now is broken)
+// javascript:void function(){var e=document.createElement("script");e.src="https://cdn.jsdelivr.net/gh/trigvi/bookmarklets/las.js",document.body.appendChild(e)}();
